b13f8829d460aa28e18ec1eeb205ed7de65717f3
mazahiddee <mazahiddee@gmail.com>
2026-05-24 13:43:46 +0800

    yes

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,11 +1,12 @@
 <?php
 
-DEFINE ('DB_HOST',     'localhost');
+DEFINE ('DB_HOST',     'mysql.railway.internal');
 DEFINE ('DB_USER',     'root');
-DEFINE ('DB_PASSWORD', '');
-DEFINE ('DB_NAME',     'cyri_drive_co');
+DEFINE ('DB_PASS', 'WcPxaesYmUdQjBcmizQuGWYhOyutIRzn');
+DEFINE ('DB_NAME',     'railway');
+DEFINE ('DB_PORT',     '3306');
 
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT)
     OR die('Could not connect to MySQL: ' . mysqli_connect_error());
 
-mysqli_set_charset($dbc, 'utf8');
+mysqli_set_charset($dbc, 'utf8');       
